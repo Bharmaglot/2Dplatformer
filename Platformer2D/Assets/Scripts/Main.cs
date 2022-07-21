b70367c84636a6b3c5631c67f040afca4a47dc75
Bharmaglot <46847706+Bharmaglot@users.noreply.github.com>
2022-07-21 12:16:56 +0300

    lesson_1
    1_1

---
+++ Platformer2D/Assets/Scripts/Main.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PlatformerMVC
+{
+    public class Main : MonoBehaviour
+    {
+        private SpriteAnimatorController _playerAnimator;
+
+        private AnimationConfig _config;
+
+        [SerializeField] private LevelObjectView _playerView; 
+
+        private void Awake()
+        {
+            _config = Resources.Load<AnimationConfig>("SpriteAnimationCfg");
+            _playerAnimator = new SpriteAnimatorController(_config);
+            _playerAnimator.StartAnimation(_playerView._renderer, AnimState.Run, true, 10);
+        }
+
+        void Update()
+        {
+            _playerAnimator.Update();
+        }
+    }
+}
---
+++ Platformer2D/Assets/Scripts/Views/LevelObjectView.cs
@@ -0,0 +1,13 @@
+using System;
+using UnityEngine;
+
+namespace PlatformerMVC
+{
+    public class LevelObjectView : MonoBehaviour
+    {
+        public Transform _transform;
+        public SpriteRenderer _renderer;
+        public Collider2D _collider;
+        public Rigidbody2D _rb;
+    }
+}
