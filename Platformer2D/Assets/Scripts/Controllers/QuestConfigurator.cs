0c14df263d2817d4870007fc776ee1168ea00def
Bharmaglot <46847706+Bharmaglot@users.noreply.github.com>
2022-08-24 12:37:19 +0300

    1
    1

--- Platformer2D/Assets/Scripts/Controllers/QuestConfigurator.cs
+++ Platformer2D/Assets/Scripts/Controllers/QuestConfigurator.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 namespace PlatformerMVC
 {
-    public class QuestConfigurator 
+    public class QuestConfigurator
     {
         private QuestObjectView _singleQuest;
         private QuestController _singleQuestController;
@@ -13,7 +13,13 @@ namespace PlatformerMVC
         private QuestObjectView[] _questObjects;
         private QuestCoinModel _model;
 
+        private List<IQuestStory> _questStories;
+
         private InteractiveObjectView _player;
+
+        private Dictionary<QuestType, Func<IQuestModel>> _questFactory = new Dictionary<QuestType, Func<IQuestModel>>(1);
+        private Dictionary<QuestStoryType, Func<List<IQuest>, IQuestStory>> _questStoryFactory = new Dictionary<QuestStoryType, Func<List<IQuest>, IQuestStory>>(2);
+
 
         public QuestConfigurator(QuestView view, InteractiveObjectView player)
         {
@@ -28,6 +34,54 @@ namespace PlatformerMVC
         {
             _singleQuestController = new QuestController(_player, _singleQuest, _model);
             _singleQuestController.Reset();
+
+            _questFactory.Add(QuestType.Coins, () => new QuestCoinModel());
+            _questStoryFactory.Add(QuestStoryType.Common, questCollection => new QuestStoryController(questCollection));
+
+            _questStories = new List<IQuestStory>();
+
+            foreach (QuestStoryConfig cfg in _storyConfig)
+            {
+                _questStories.Add(CreateQuestStory(cfg));
+            }
+        }
+        private IQuest CreateQuest(QuestConfig cfg)
+        {
+            int questID = cfg.id;
+
+            QuestObjectView qView = _questObjects.FirstOrDefault(value => value._id == cfg.id);
+
+            if (qView == null)
+            {
+                Debug.Log("NoView");
+                return null;
+            }
+
+            if (_questFactory.TryGetValue(cfg.type, out var factory))
+            {
+                IQuestModel qModel = factory.Invoke();
+                return new QuestController(_player, qView, qModel);
+            }
+
+            Debug.Log("NoModel");
+            return null;
+        }
+
+        private IQuestStory CreateQuestStory(QuestStoryConfig cfg)
+        {
+            List<IQuest> quests = new List<IQuest>();
+
+            foreach (QuestConfig Qcfg in cfg.quests)
+            {
+                IQuest quest = CreateQuest(Qcfg);
+
+                if (quest == null) continue;
+
+                quests.Add(quest);
+                Debug.Log("Add Quest");
+            }
+
+            return _questStoryFactory[cfg.type].Invoke(quests);
         }
     }
 }
--- Platformer2D/Assets/Scripts/Controllers/QuestStoryController.cs
+++ Platformer2D/Assets/Scripts/Controllers/QuestStoryController.cs
@@ -1,18 +1,78 @@
-using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class QuestStoryController : MonoBehaviour
+namespace PlatformerMVC
 {
-    // Start is called before the first frame update
-    void Start()
+    public class QuestStoryController : IQuestStory
     {
-        
-    }
+        private List<IQuest> _questCollection = new List<IQuest>();
 
-    // Update is called once per frame
-    void Update()
-    {
+        public bool IsDone => _questCollection.All(value => value.IsCompleted);
+
+        public QuestStoryController(List<IQuest> questCollection)
+        {
+            _questCollection = questCollection;
+            Subscribe();
+            Reset(0);
+        }
         
+        private void Subscribe()
+        {
+            foreach(IQuest quest in _questCollection)
+            {
+                quest.QuestCompleted += OnQuestCompleted;
+            }
+        }
+
+        private void Unsubscribe()
+        {
+            foreach (IQuest quest in _questCollection)
+            {
+                quest.QuestCompleted -= OnQuestCompleted;
+            }
+        }
+
+        public void Reset(int index)
+        {
+            if(index < 0||index > _questCollection.Count)
+            {
+                return;
+            }
+
+            IQuest quest = _questCollection[index];
+
+            if(quest.IsCompleted)
+            {
+                OnQuestCompleted(this, quest);
+            }
+            else
+            {
+                quest.Reset();
+            }
+
+        }
+
+        private void OnQuestCompleted(object sender, IQuest quest)
+        {
+            int index = _questCollection.IndexOf(quest);
+            if(IsDone)
+            {
+                Debug.Log("Story is done");
+            }
+            else
+            {
+                Reset(++index);
+            }
+        }
+
+        public void Dispose()
+        {
+            Unsubscribe();
+            foreach (var item in _questCollection)
+            {
+                item.Dispose();
+            }
+        }
     }
 }
